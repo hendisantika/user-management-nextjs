7076c683ab4b9d601874db911d60d87768ac935d
hendisantika <hendisantika@yahoo.co.id>
2024-06-06 14:53:45 +0700

    Add deleteEmployee in UserController🫸🌀✏️📗🐧🐳

--- user-service/src/main/java/id/my/hendisantika/userservice/controller/UserController.java
+++ user-service/src/main/java/id/my/hendisantika/userservice/controller/UserController.java
@@ -5,6 +5,7 @@ import id.my.hendisantika.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -49,4 +52,12 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable("id") Long id) {
+        boolean deleted = false;
+        deleted = userService.deleteUser(id);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", deleted);
+        return ResponseEntity.ok(response);
+    }
 }
