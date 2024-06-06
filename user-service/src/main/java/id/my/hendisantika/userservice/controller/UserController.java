609c44e608a9607bd13debb438c13259ec23424d
hendisantika <hendisantika@yahoo.co.id>
2024-06-06 14:54:09 +0700

    Add updateUser in UserController🫸🌀✏️📗🐧🐳

--- user-service/src/main/java/id/my/hendisantika/userservice/controller/UserController.java
+++ user-service/src/main/java/id/my/hendisantika/userservice/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -59,5 +60,12 @@ public class UserController {
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", deleted);
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable("id") Long id,
+                                              @RequestBody UserDTO user) {
+        user = userService.updateUser(id, user);
+        return ResponseEntity.ok(user);
     }
 }
