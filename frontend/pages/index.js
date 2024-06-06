6bc1f9fb7cad974a8d5b0cbcbccf87ce773c0a2c
hendisantika <hendisantika@yahoo.co.id>
2024-06-06 15:07:41 +0700

    Add apis🫸🌀✏️📗🐧🐳

---
+++ frontend/pages/index.js
@@ -0,0 +1,28 @@
+import {getSession} from "next-auth/react";
+import Head from "next/head";
+import AddUser from "../components/AddUser";
+import Login from "../components/Login";
+import Navbar from "../components/Navbar";
+
+export default function Home({session}) {
+    if (!session) return <Login/>;
+    return (
+        <div>
+            <Head>
+                <title>User management App</title>
+            </Head>
+            <Navbar/>
+
+            <main>
+                <AddUser/>
+            </main>
+        </div>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+    return {
+        props: {session},
+    };
+}
