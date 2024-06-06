dd06619231277c2101f169a401017f0f8e61728d
hendisantika <hendisantika@yahoo.co.id>
2024-06-06 14:57:45 +0700

    Modify next.config.mjs🫸🌀✏️📗🐧🐳

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ["platform-lookaside.fbsbx.com"],
+    },
+};
 
 export default nextConfig;
